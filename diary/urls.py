718b3afba5e82adb0d1f3d4f6e8c0e9db5c932d1
fujishiro <ksw2070168@stu.o-hara.ac.jp>
2021-09-13 12:42:35 +0900

    メディアファイルを使えるようにする

--- diary/urls.py
+++ diary/urls.py
@@ -13,11 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.contrib.staticfiles.urls import static
+from django.urls import path,include
 
-app_name = 'diary'
+from . import settings_common, settings_dev
+
 urlpatterns = [
-    path('',views.IndexView.as_view(),name='index'),
-    path('inquiry/', views.InquiryView.as_view(),name="inquiry")
+    path('admin/',admin.site.urls),
+    path("",include('diary.urls')),
+    path('accounts/',include('allauth.urls')),
 ]
+
+urlpatterns += static(settings_common.MEDIA_URL,document_root=settings_dev.MEDIA_ROOT)
